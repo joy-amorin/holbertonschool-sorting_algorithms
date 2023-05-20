ab86d38b37d102df0b389f9e71d286d58a4a4722
Joy-Amorin <amorinkaren2021@gmail.com>
2023-05-20 10:55:17 -0700

    select

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+*selection_sort - sorts an array of integer using Selection sort algorithm
+*@array: array to sort
+*@size: size of array
+*
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		/*swap*/
+
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+
+
+	}
+}
